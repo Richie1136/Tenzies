a85b4f204c3cca984761097e1734dc052d66f873
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-02-18 09:32:15 -0500

    Keeping the held values that i clicked on when going to re roll the die

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -7,22 +7,31 @@ function App() {
   const allNewDice = () => {
     let result = []
     for (let i = 0; i < 10; i++) {
-      result.push({ value: Math.floor(Math.random() * 6) + 1, isHeld: false, id: nanoid() })
+      result.push(generateNewDie())
     }
     return result
   }
 
+  const generateNewDie = () => {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   const [numbers, setNumbers] = useState(allNewDice())
 
-  const rollDice = () => {
-    setNumbers(allNewDice())
+  const rollDice = (id) => {
+    setNumbers(oldDice => oldDice.map((num) => {
+      return num.isHeld ? num : generateNewDie()
+    }))
   }
 
   const holdDice = (id) => {
-    console.log(id)
     setNumbers(oldDice => oldDice.map((num) => {
       return num.id === id ?
-        { ...num, isHeld: !num.isHeld } : { ...num }
+        { ...num, isHeld: !num.isHeld } : num
     }))
   }
 
